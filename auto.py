3e8ae8e23784a76fd1dfbabaa5dba7b46cf13cb1
tlatkdgus1 <tlatkdgus1@gmail.com>
2018-04-25 19:21:16 +0900

    edited

--- auto.py
+++ auto.py
@@ -1,7 +1,5 @@
 import subprocess
 import sys
-import pexpect
-import re
 
 def makeAccount():
     process = pexpect.spawn('geth --datadir /home/pi-lab/data account new')
@@ -10,14 +8,3 @@ def makeAccount():
     process.expect("assphrase:")
     process.sendline("1234qwer")
     account = process.read().split("{")[1].split("}")[0]
-    return account
-
-def editJson(account):
-    with open('/home/pi-lab/genesis.json/genesis.json') as genesis:
-            lines = genesis.readlines()
-    lines[10] = str(re.sub('0x(.*)',"0x"+account+"\": {", lines[10]))
-    with open('/home/pi-lab/genesis.json/genesis.json', 'w') as genesis:
-        genesis.writelines(lines)
-
-account = makeAccount()
-editJson(account)
