7ece614edac26e5377125f3b8de22d5508c61272
tlatkdgus1 <tlatkdgus1@gmail.com>
2018-04-26 16:30:22 +0900

    edited script.py

---
+++ script.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+import pexpect
+def openGeth(enode):
+    process = pexpect.spawn('geth --rpc --datadir "/home/pi-lab/data" --rpcaddr 0.0.0.0 --rpcport 8545 --rpccorsdomain "*" --nodiscover console')
+    process.expect(">")
+    process.sendline("personal.unlockAccount(eth.accounts[0], '1234qwer', 0)");
+    process.expect(">")
+    process.sendline("admin.addPeer(\""+enode+"\")")
+    process.interactive()
+
+enode = raw_input('Input enode : ')
+openGeth(enode)
+
